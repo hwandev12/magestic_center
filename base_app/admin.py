e736cc383637ac800cd5c8017efd008c00f4515b
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-08 16:08:42 +0500

    profile post save

--- base_app/admin.py
+++ base_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Candidate, Agent, User
+from .models import Candidate, Agent, User, UserProfile
 
 admin.site.register(Candidate)
+admin.site.register(UserProfile)
 admin.site.register(Agent)
 admin.site.register(User)
 
--- base_app/models.py
+++ base_app/models.py
@@ -1,10 +1,17 @@
 from calendar import c
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     pass
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.user.username)
 class Candidate(models.Model):
     
     class Meta:
@@ -23,6 +30,14 @@ class Candidate(models.Model):
     
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     
     def __str__(self):
         return str(self.user)
+
+
+def create_post_save(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)    
+
+post_save.connect(create_post_save, sender=User)
