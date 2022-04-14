a4bd88eb8fc75db57b8ac3c0b4596e8109254a50
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-14 20:38:15 +0500

    password reset view

--- core/urls.py
+++ core/urls.py
@@ -3,12 +3,15 @@ from django.urls import path, include
 from base_app.views import BaseView, SignupView
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 
+app_name = 'main'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', BaseView.as_view(), name='home'),
     path('', include('base_app.urls', namespace='candidate')),
     path('agents/', include('agents.urls', namespace='agents')),
     path('login/', LoginView.as_view(), name='login'),
+    path('passwordResetView/', PasswordResetView.as_view(), name='password_reset_view'),
     path('register/', SignupView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
