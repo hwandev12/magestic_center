1fb8ca237c63aa3f41740084c7273b6319dc23ed
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-17 20:50:07 +0500

    agent assign form 1-part

--- base_app/form.py
+++ base_app/form.py
@@ -1,9 +1,10 @@
 from dataclasses import fields
 from email.policy import default
+from urllib import request
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UsernameField
-from .models import Candidate
+from .models import Candidate, Agent
 
 User = get_user_model()
 
@@ -31,3 +32,13 @@ class NewUserForm(UserCreationForm):
         model = User
         fields = ("username",)
         field_classes = {"username": UsernameField}
+
+# Bu AssignaAgentForm da agenti yo'qni aniqlash va tayinlash uchun forma 
+class AssignAgentForm(forms.Form):
+    agent = forms.ModelChoiceField(queryset=Agent.objects.null())
+
+    def __init__(self, *args, **kwargs):
+        request = kwargs.pop('request')
+        agents = Agent.objects.filter(organiser=request.user.userprofile)
+        super(AssignAgentForm, self).__init__(*args, **kwargs)
+        self.fields['agent'].queryset = agents
