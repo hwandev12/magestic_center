20addaa197b9d9fcdcf4b9c55912c13a4dd31162
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 11:24:52 +0500

    add category to the admin

--- base_app/admin.py
+++ base_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Candidate, Agent, User, UserProfile
+from .models import Candidate, Agent, User, UserProfile, Category
 
 admin.site.register(Candidate)
 admin.site.register(UserProfile)
 admin.site.register(Agent)
 admin.site.register(User)
-
+admin.site.register(Category)
