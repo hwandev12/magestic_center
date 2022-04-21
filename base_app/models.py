bea35e80c2ea4d6acb99b44d0025e8fcc6ae3931
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-21 11:23:52 +0500

    add category model

--- base_app/models.py
+++ base_app/models.py
@@ -6,38 +6,51 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     is_organiser = models.BooleanField(default=True)
     is_agent = models.BooleanField(default=False)
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return str(self.user.username)
+
+
 class Candidate(models.Model):
-    
+
     class Meta:
         verbose_name = 'Candidate'
         verbose_name_plural = 'Candidates Admittes'
-        
+
     name = models.CharField(max_length=30)
     lastName = models.CharField(max_length=40)
     age = models.IntegerField(default=0)
     email = models.EmailField()
     job = models.CharField(max_length=20)
     organiser = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    agent = models.ForeignKey("Agent", null=True, blank=True, on_delete=models.SET_NULL)
-    
+    agent = models.ForeignKey(
+        "Agent", null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+
     def __str__(self):
         return self.name
-    
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     organiser = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return str(self.user)
 
 
 def create_post_save(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)    
+        UserProfile.objects.create(user=instance)
+
 
 post_save.connect(create_post_save, sender=User)
