a0f62c86f03bfc402eacea3819b95b02a40477d5
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-23 04:21:55 +0500

    problems and solutions

--- base_app/urls.py
+++ base_app/urls.py
@@ -6,6 +6,8 @@ app_name = 'candidate'
 
 urlpatterns = [
     path('<int:pk>/', Candidate_details.as_view(), name='details'),
+    path('category-lists/<int:pk>/',
+         AssignCategoryDetailsView.as_view(), name='category_detail'),
     path('<int:pk>/update/', Update_candidate.as_view(), name='update'),
     path('<int:pk>/delete/', Delete_candidate.as_view(), name='delete'),
     path('<int:pk>/assign-agent/', AgentAssignView.as_view(), name='assign_agent'),
@@ -13,6 +15,4 @@ urlpatterns = [
     path('create/', Candidate_create.as_view(), name='create'),
     path('deleted/', Deleted.as_view(), name='deleted'),
     path('category-lists/', CategoryAssignView.as_view(), name='category'),
-    path('category-lists/<int:pk>/',
-         AssignCategoryDetailsView.as_view(), name='category_detail'),
 ]
