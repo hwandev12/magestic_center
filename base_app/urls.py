7dfd15d4b384fca62e150a0f58f2507eb1c5925e
hwandev12 <husan.ahmedov777@yandex.com>
2022-04-23 06:10:07 +0500

    add category update url and some issues solve

--- base_app/urls.py
+++ base_app/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('<int:pk>/update/', Update_candidate.as_view(), name='update'),
     path('<int:pk>/delete/', Delete_candidate.as_view(), name='delete'),
     path('<int:pk>/assign-agent/', AgentAssignView.as_view(), name='assign_agent'),
+    path('<int:pk>/category-update-detail/',
+         CandidateCategoryUpdateView.as_view(), name='category_update_detail'),
     path('candidate/', Candidate_lists.as_view(), name='candidate'),
     path('create/', Candidate_create.as_view(), name='create'),
     path('deleted/', Deleted.as_view(), name='deleted'),
     path('category-lists/', CategoryAssignView.as_view(), name='category'),
     path('category-lists/<int:pk>/',
          AssignCategoryDetailsView.as_view(), name='category_detail'),
-    path('<int:pk>/category-update/',
-         CandidateCategoryUpdateView.as_view(), name='category_update')
 ]
